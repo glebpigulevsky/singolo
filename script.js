ff9cdb9833394bc9b395fe584cab109640922a6f
Gleb Pigulevsky <pigulevsky.gleb@ya.ru>
2023-04-12 07:24:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// const anchors = document.querySelectorAll('a[href*="#"]')
-
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
-    
-//     const blockID = anchor.getAttribute('href').substr(1)
-    
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
 document.addEventListener('scroll', onScroll);
 function onScroll(event){
     const currPos = window.scrollY;
@@ -19,11 +5,7 @@ function onScroll(event){
     
     const links = document.querySelectorAll('#menu>a');
     const linksMobile = document.querySelectorAll('ul.mobilMenuList a');
-    //console.log(linksMobile);
-    //console.log(currPos);
     document.querySelectorAll('body>div').forEach((el) => {
-        //console.log(el);
-        //debugger;
         if(el.offsetTop-64 <= currPos && (el.offsetTop +el.offsetHeight) >currPos){
             linksMobile.forEach((a) =>{
                 a.classList.remove('activeMenuTag');
@@ -49,8 +31,6 @@ function onScroll(event){
 window.onload = function() {
     addSelectRedTagMenu();
     addSelectedPortfolioTag();
-    //setInterval();
-    //turnScreenPhoneVert();
     addSelectRedPortfolioImage();
 }
 
@@ -110,158 +90,6 @@ const  addActivePortfolioTag = (clickedPortfolioItem) => {
 addSelectRedTagMenu();
 addSelectedPortfolioTag();
 
-
-//-------------SLIDER
-
-// let counter = 0;
-// let interval = setInterval(function(){
-//     document.querySelectorAll('.itemSlider').forEach((item) => {
-//         if(item.style.left === '-795px'){
-//             clearInterval(interval);
-//             return;
-//         }
-//         counter -=1;
-//         item.style.left = `${counter}px`;
-//     });
-// },0.05);
-
-// let slider =document.querySelector('.slider__Container'),
-//     slides = slider.querySelectorAll('.itemSlider'),
-//     len = slides.length,
-//     index = len - 1,
-//     dir = -1;
-//     (function move(){
-//         slides[index].style.left='';
-//         index = (index + dir + len) % len;
-//         slides[index].style.left = dir == -1 ? "100%" : "-100%";
-//         window.setTimeout(move, 5000);
-
-//     })();
-// (function() {
-
-//     var doc = document,
-//         index = 1;
-
-//     var Slider = function() {
-//         this.box = doc.querySelector('.carousel-container');
-//         this.slidesBox = doc.querySelector('.carousel-slides');
-//         this.slides = doc.querySelectorAll('.slide');
-//         this.btns = doc.querySelectorAll('.btn');
-//         this.size = this.box.clientWidth;
-
-//         this.position();
-//         this.carousel();
-
-//     };
-
-//     Slider.prototype.position = function() {
-//         var size = this.size;
-//         this.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-//     };
-
-//     Slider.prototype.carousel = function() {
-//         var i, max = this.btns.length,
-//             that = this;
-
-//         for (i = 0; i < max; i += 1) {
-//             that.btns[i].addEventListener('click', Slider[that.btns[i].id].bind(null, that));
-//         }
-//     }
-
-//     Slider.prev = function(box) {
-//         box.slidesBox.style.transition = "transform .3s ease-in-out";
-//         //document.querySelector("body > div.sliderWidth").style.backgroundColor="#648BF0";
-//         var size = box.size;
-//         index <= 0 ? false : index--;
-//         box.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-//         box.jump();
-//     };
-
-//     Slider.next = function(box) {
-//         box.slidesBox.style.transition = "transform .3s ease-in-out";
-//         var max = box.slides.length;
-//         var size = box.size;
-//         index >= max - 1 ? false : index++;
-//         box.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-//         box.jump();
-//     };
-
-//     Slider.prototype.jump = function() {
-//         var that = this;
-//         var size = this.size;
-//         this.slidesBox.addEventListener('transitionend', function() {
-//             that.slides[index].id === "firstClone" ? index = 1 : index;
-//             that.slides[index].id === "lastClone" ? index = that.slides.length - 2 : index;
-//             that.slidesBox.style.transition = "none";
-//             that.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-//         });
-//     }
-
-
-//     new Slider();
-
-// })();
-
-// let sliderBlock = document.querySelector("#sliderBlock");
-// (function(){
-//     document.querySelector("#prev").addEventListener('click', (e) => {  
-//         if (sliderBlock.classList.contains('sliderColorRed')){
-//             sliderBlock.classList.remove('sliderColorRed');
-//             sliderBlock.classList.add('sliderColorBlue');
-//         } else if(sliderBlock.classList.contains('sliderColorBlue')){
-//             sliderBlock.classList.remove('sliderColorBlue');
-//             sliderBlock.classList.add('sliderColorRed');
-//         }
-//     })
-// })();
-
-
-// (function(){
-//     document.querySelector("#next").addEventListener('click', (e) => {  
-//         if (sliderBlock.classList.contains('sliderColorRed')){
-//             sliderBlock.classList.remove('sliderColorRed');
-//             sliderBlock.classList.add('sliderColorBlue');
-//         } else if(sliderBlock.classList.contains('sliderColorBlue')){
-//             sliderBlock.classList.remove('sliderColorBlue');
-//             sliderBlock.classList.add('sliderColorRed');
-//         }
-//     })
-// })();
-
-
-
-//----------выключение экрана телефона при нажатии.---------//
-// let blackScreenVert = document.querySelector("#blackScreenVert");
-// (function(){
-//     document.querySelector("#slideSec > div:nth-child(1)").addEventListener('click', (e) => {  
-//         console.log(e.target);
-//         if (blackScreenVert.classList.contains('hiddenScreen')){
-//             blackScreenVert.classList.remove('hiddenScreen');
-//             blackScreenVert.classList.add('activeScreen');
-//         } else if(blackScreenVert.classList.contains('activeScreen')){
-//             blackScreenVert.classList.remove('activeScreen');
-//             blackScreenVert.classList.add('hiddenScreen');
-//         }
-//     })
-// })();
-// let blackScreenHor = document.querySelector("#blackScreenHor");
-// (function(){
-//     document.querySelector("#slideSec > div:nth-child(2) > img").addEventListener('click', (e) => {
-        
-//         if (blackScreenHor.classList.contains('hidden')){
-//             blackScreenHor.classList.remove('hidden');
-//             blackScreenHor.classList.add('visible');
-//         } else if(blackScreenHor.classList.contains('visible')){
-//             blackScreenHor.classList.remove('visible');
-//             blackScreenHor.classList.add('hidden');
-//         }
-        
-        
-//     })
-// })();
-
-
-
 let blackScreenHor = document.querySelector("#blackScreenHor");
 function switchOnOffScreenHor(){
     let blackScreenHor = document.querySelector("#blackScreenHor");
@@ -284,36 +112,8 @@ function switchOnOffScreenVert(){
         blackScreenVert.classList.add('hiddenScreen');
     }
 }
-// (function(){
-//     document.querySelector("#slideSec > div:nth-child(1)").addEventListener('click', (e) => {  
-//         console.log(e.target);
-//         if (blackScreenVert.classList.contains('hidden')){
-//             blackScreenVert.classList.remove('hidden');
-//             blackScreenVert.classList.add('visible');
-//         } else if(blackScreenVert.classList.contains('visible')){
-//             blackScreenVert.classList.remove('visible');
-//             blackScreenVert.classList.add('hidden');
-//         }
-//     })
-// })();
-// let blackScreenHor = document.querySelector("#blackScreenHor");
-// (function(){
-//     document.querySelector("#slideSec > div:nth-child(2) > img").addEventListener('click', (e) => {
-        
-//         if (blackScreenHor.classList.contains('hidden')){
-//             blackScreenHor.classList.remove('hidden');
-//             blackScreenHor.classList.add('visible');
-//         } else if(blackScreenHor.classList.contains('visible')){
-//             blackScreenHor.classList.remove('visible');
-//             blackScreenHor.classList.add('hidden');
-//         }
-        
-        
-//     })
-// })();
 
-//----------перемешивание картинок в блоке портфолио-------//
-const mixingPortfolioImages = () =>{
+const mixingPortfolioImages = () => {
     let portfolioGrid = document.querySelector(".portfolio > div > div");
     portfolioGrid.prepend(portfolioGrid.lastElementChild);
 };
